8b19917f89580b3fcd719cc8a0644ef4d357c8c5
PRAKALYARAMESH <prakalyars.21ece@kongu.edu>
2024-01-26 20:42:13 +0530

    prakalya

--- src/components/functionalcomponents/About.jsx
+++ src/components/functionalcomponents/About.jsx
@@ -1,8 +1,30 @@
+import React, {useEffect, useState} from "react";
+
 const About=()=>{
+    const[tabName,setTabName]=useState("You are free from cold don't take any pills to avoid side effects.")
+    const updateTablet=()=>{
+        setTabName("Welcome to cold club take this tablet citracin to cure it. ");
+    }
+    const updateTablet1=()=>{
+        setTabName("Welcome to fever club take this tablet Dolo-650 to cure it. ");
+    }
+    const updateTablet2=()=>{
+        setTabName("Welcome to epilepsy club take this tablet Levipil to cure it. ");
+    }
+
+    useEffect(()=>{
+        console.log("Side effects are sleepy and drowsiness");
+    },[]);
+
     return(
         <div>
-            <h1>This is my about Page</h1>
+            <h1>{tabName}</h1>
+            <button onClick={updateTablet}>Click me if you get cold</button>
+            <button onClick={updateTablet1}>Click me if you get fever</button>
+            <button onClick={updateTablet2}>Click me if you get epilepsy</button>
+
         </div>
     )
 }
+
 export default About;
--- src/components/functionalcomponents/NavBar.jsx
+++ src/components/functionalcomponents/NavBar.jsx
@@ -1,8 +1,10 @@
+
 import "../functionalcomponents/NavBar.css"
 import { Link } from "react-router-dom";
 const NavBar=()=>{
     return(
         <ol>
+            
             <li><Link to="/Home">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/experience">Experience</Link></li>
